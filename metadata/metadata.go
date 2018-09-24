3abdcea8849b3eeeb6c7e911f0d340a7a4604b71
Sai Kiran Dasika <kirandasika30@gmail.com>
2018-09-24 13:11:44 -0500

    new metadata package for global config management

---
+++ metadata/metadata.go
@@ -0,0 +1,126 @@
+package metadata
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+type SponsorLevel string
+
+const (
+	Kilo SponsorLevel = "kilo"
+	Mega SponsorLevel = "mega"
+	Giga SponsorLevel = "giga"
+	Tera SponsorLevel = "tera"
+	Aux  SponsorLevel = "aux"
+)
+
+type AuburnHacks struct {
+	metaFileURL string
+	// mu gaurds all the variables below.
+	mu sync.RWMutex
+
+	AboutUs     string            `json:"about_us"`
+	InfoCards   []*InfoCard       `json:"info_cards"`
+	Sponsors    []*Sponsor        `json:"sponsors"`
+	SocialMedia map[string]string `json:"social_media"`
+}
+
+type InfoCard struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
+type Sponsor struct {
+	Name  string       `json:"name"`
+	Logo  string       `json:"logo"`
+	Level SponsorLevel `json:"level"`
+}
+
+func New(metaFileURL string) *AuburnHacks {
+	auHack := &AuburnHacks{
+		metaFileURL: metaFileURL,
+	}
+
+	// not updating metadata in this function as the watch
+	// go routine must be called immediately after object
+	// allocation
+
+	return auHack
+}
+
+// Watch is a function that runs as a goroutine and polls a
+// metadata file on google drive
+func (c *AuburnHacks) Watch(d time.Duration) {
+	for {
+		glog.V(2).Infof("polling metadata...")
+
+		metadata, err := c.getMetadata()
+		if err != nil {
+			glog.Errorf("error getting metadata: %v", err)
+			continue
+		}
+
+		if err = c.updateMetadata(metadata); err != nil {
+			glog.Errorf("error updating metadata: %v", err)
+			continue
+		}
+
+		glog.V(2).Infof("raw metadata: %s", metadata)
+		glog.V(2).Infof("struct after: %+v", c)
+
+		time.Sleep(d)
+	}
+}
+
+func (c *AuburnHacks) RLock() {
+	c.mu.RLock()
+}
+
+func (c *AuburnHacks) RUnlock() {
+	c.mu.RUnlock()
+}
+
+func (c *AuburnHacks) updateMetadata(newData []byte) error {
+	// grab the lock
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	newMeta := New(c.metaFileURL)
+	if err := json.Unmarshal(newData, newMeta); err != nil {
+		return err
+	}
+
+	// update the stuff
+	c.AboutUs = newMeta.AboutUs
+	c.InfoCards = newMeta.InfoCards
+	c.Sponsors = newMeta.Sponsors
+	c.SocialMedia = newMeta.SocialMedia
+
+	return nil
+}
+
+func (c *AuburnHacks) getMetadata() ([]byte, error) {
+	hc := http.Client{}
+	req, err := http.NewRequest("GET", c.metaFileURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := hc.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bb, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return bb, nil
+}
--- server.go
+++ server.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"auburn-hacks-landing/metadata"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+
+	"github.com/golang/glog"
 )
 
 var (
-	staticDir  *string
-	listenAddr *string
+	staticDir    *string
+	listenAddr   *string
+	pollDuration *time.Duration
+
+	meta *metadata.AuburnHacks
+)
+
+const (
+	MetaFileURL = "https://drive.google.com/uc?id=1AXg6vBbyZ4XR7m8skvQ_062ZEgfGdGvX&export=download"
 )
 
 func init() {
 	staticDir = flag.String("static_dir", "static", "static files directory")
 	listenAddr = flag.String("listen_addr", "localhost:8321", "http serve address")
+	pollDuration = flag.Duration("poll_duration", 5, "poll duration for metadata watch")
+
+	flag.Set("v", "0")
+	flag.Set("logtostderr", "true")
 
 	flag.Parse()
 }
@@ -24,8 +41,16 @@ func init() {
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(*staticDir)))
 	http.HandleFunc("/healthz", health)
+	http.HandleFunc("/metadata", metadataHandler)
 
-	log.Printf("server listening on %s pid: %d", *listenAddr, os.Getpid())
+	glog.Infof("starting server on %s pid: %d", *listenAddr, os.Getpid())
+
+	// global metadata object
+	meta = metadata.New(MetaFileURL)
+
+	// start watching the file
+	go meta.Watch(*pollDuration * time.Minute)
+
 	c := make(chan os.Signal)
 	signal.Notify(c, syscall.SIGTERM, os.Interrupt)
 	go func() {
@@ -39,4 +64,19 @@ func main() {
 func health(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte("ok"))
+}
+
+func metadataHandler(w http.ResponseWriter, r *http.Request) {
+	// allowing parallel reads
+	meta.RLock()
+	defer meta.RUnlock()
+	w.Header().Set("Content-Type", "application/json")
+
+	bb, err := json.Marshal(meta)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("json marshal error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(bb)
 }
